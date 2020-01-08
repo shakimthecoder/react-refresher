ea4c2fc5b7077c8242e849cafdc1fc07df6a6ee6
amenraprod <amenraprod@gmail.com>
2020-01-08 13:42:44 -0500

    setup state

--- src/App.js
+++ src/App.js
@@ -3,22 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render(){
+ constructor() {
+ super();
+
+ this.state = {
+   string: 'Shakim is a millionaire'
+ };
+}
+    
+  render() {
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+            <button onClick={() => this.setState({ string: 'Shakim is rich' })}>
+            Shakim is amazing
+            </button>
         </header>
       </div>
     );
