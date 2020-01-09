90e9f0931b97e5d313c11eb1e1468eba85092ddc
amenraprod <amenraprod@gmail.com>
2020-01-09 15:05:59 -0500

    passed props to searchbox

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/searchbox.component';
 
 class App extends Component {
  constructor() {
@@ -25,10 +26,10 @@ response.json())
     );
     return (
       <div className="App">
-       <input type='search' 
-         placeholder='search' 
-         onChange={e => this.setState({ searchField: e.target.value})}>
-      </input>
+        <SearchBox
+        placeholder='search monsters'
+        handleChange={e => this.setState({ searchField: e.target.value})}
+        />
         <CardList monsters={filteredMonsters}>
         
         </CardList>
