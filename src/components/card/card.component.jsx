902d911604a8641ff06017fe3c4b0db94c18b63e
amenraprod <amenraprod@gmail.com>
2020-01-09 13:33:13 -0500

    added email

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -5,5 +5,6 @@ export const Card = props => (
     <div className="card-container">
         <img alt='monster' src={`https://robohash.org/${props.monster.id}?set=set2`} />
         <h2> {props.monster.name} </h2>
+        <h2> {props.monster.email} </h2>
     </div>
 )
