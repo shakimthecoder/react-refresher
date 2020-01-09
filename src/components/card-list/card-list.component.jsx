32064c5000a3d4e3e20864c6439919cae4ea2c91
amenraprod <amenraprod@gmail.com>
2020-01-09 12:43:04 -0500

    updated props

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import './card-list.styles.css';
 
-export const CardList = props => {
-    
-    return(<div className="card-list">
-        {props.monsters.map(monster => <h1 key={monster.id}> {monster.name} </h1>)}
-        </div>)}
+export const CardList = props => (<div className="card-list">
+    {props.monsters.map(monster => <h1 key={monster.id}> {monster.name} </h1>)}
+</div>)
