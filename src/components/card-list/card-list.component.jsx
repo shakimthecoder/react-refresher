ac87f9c6bdf1ed7267b525365382c4dbbe1cbb1f
amenraprod <amenraprod@gmail.com>
2020-01-09 13:18:11 -0500

    added card and passed monster prop

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Card } from '../card/card.component';
 import './card-list.styles.css';
 
 export const CardList = props => (<div className="card-list">
-    {props.monsters.map(monster => <h1 key={monster.id}> {monster.name} </h1>)}
+    {props.monsters.map(monster => (
+        <Card key={monster.id} monster={monster} /> ))}
 </div>)
